cb997385d7319904abc147629cd0903f6bccff46
youngartist-lang <katrys2605@gmail.com>
2026-07-23 12:42:49 +0300

    First Program_1

---
+++ prog1_23.07.py
@@ -0,0 +1,23 @@
+# player_name = "Ніндзя"
+# print(player_name)
+
+# player_name = "Mar"
+# print(player_name)
+
+# age_1 = 25
+# print(age_1)
+
+# Age_1 = 30
+# print(Age_1)
+
+# x = 6
+# y = 3
+# z = x + y
+# print(z)
+
+# email = ""
+# print(email)
+
+name = input("Введіть свое ім'я: ")
+print(f"Привіт, {name}")
+
